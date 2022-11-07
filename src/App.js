739894634b6ad1222da1cc78761a799e78346534
Macedo Daniel (CaP/ETS) <mdc1ca@bosch.com>
2022-11-07 16:20:06 -0300

    Adding login screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './css/App.css';
 import DashBoard from './Telas/dasboard';
 import Pagina_Inicial from './Telas/paginaInicial';
+import Login from './Telas/login';
 
 export default function App() {
   return (
     <>
+    <Login/>
     {/* <DashBoard/> */}
-    <Pagina_Inicial/>
+    {/* <Pagina_Inicial/> */}
     </>
   );
 }
---
+++ src/Telas/login.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+function Login() {
+    return (<>
+
+    <div className="principal">
+        <div className="cardLogin">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+
+        </div>
+    </div>
+
+    </>);
+}
+
+export default Login;
